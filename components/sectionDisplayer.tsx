b10b54bb4cf5a797d5276b097cff7ed8edb0ec3b
nahomkasa999 <nahomkasa345@gmail.com>
2025-04-22 14:56:27 -0400

    sectionDisplay created

--- components/sectionDisplayer.tsx
+++ components/sectionDisplayer.tsx
@@ -1,41 +1,9 @@
-import React from "react";
-import prisma from "@/lib/prisma";
+import React from 'react'
 
-async function SectionDisplayer() {
-  await prisma.$connect();
-
-  const sections = await prisma.section.findMany({
-    where: {
-      courseId: "cm9bcf9x90006e2ylxxrda4fa",
-    },
-    include: {
-      pdfs: true,
-      videos: true,
-      test: true,
-    },
-  });
-
+function sectionDisplayer() {
   return (
-    /// displaying the sections
-    <div className="flex flex-col items-center justify-center h-screen bg-white text-black">
-      <h1 className="text-2xl font-bold mb-4">Sections</h1>
-      <div className="flex flex-col w-full max-w-md">
-        {sections.map((section) => (
-          <div key={section.id} className="border border-gray-800 p-2 mb-4">
-            <h2 className="text-xl font-bold">{section.title}</h2>
-            <p>
-              PDF: {section.pdfs.map((pdf) => pdf.cloudinaryUrl).join(", ")}
-            </p>
-            <p>
-              Video:{" "}
-              {section.videos.map((video) => video.cloudinaryUrl).join(", ")}
-            </p>
-            <p>Test: {section.test?.questions}</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    <div>sectionDisplayer</div>
+  )
 }
 
-export default SectionDisplayer;
+export default sectionDisplayer
